4099781261929f190e5f047c1f7dc1f495f5fd2b
PULIASHWINI <srinupuli394@gmail.com>
2025-06-28 12:33:03 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,14 @@
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        
+        int number1 = 10;
+        int number2 = 25;
+
+        
+        int sum = number1 + number2;
+
+        // Print the result to the console
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
